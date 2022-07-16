f7871fb194bd443de2c40a0ac19a9a031f1e6a88
fantagics <libroid@naver.com>
2022-07-16 23:24:56 +0900

    swift commit update

--- swift_Study/TIL_S01_TypeOfVarLet.playground/Contents.swift
+++ swift_Study/TIL_S01_TypeOfVarLet.playground/Contents.swift
@@ -10,8 +10,21 @@ print(name)
 dump(name)
 print("My name is \(name).")
 
+//: var, let
+let a = 1
+//a = 2  //let은 변경 불가
+var aa = 1
+aa = 2
+
+//: 타입추론(Type Inference) : 자료형 타입 추론
+let one = 5 //Int
+let two = 3.4 //Double
+let three = "A" //String
+print(type(of: three))  //String
+//: 타입어노테이션(Type Annotation): 자료형 직접 표기
+let four: String = "Hello, Swift!"
+
 //: 자료형
-let a = 1  //let은 변경 불가 ( a=2 불가)
 var b : Int = -1  // 정수
 var c : UInt = 5  // 양의 정수
 var d : Float = 5.0  // 32비트 부동소수형
@@ -19,7 +32,33 @@ var e : Double = 3.2  // 64비트 부동소수형
 var f : Character = "a"  // 문자형, 큰따옴표 사용
 var g : String = "abc"  // 문자형, 큰따옴표 사용
 
-var h = 5 //자동 Int
-var i = 5.0 //자동 Double
-var j = "c" //자동 String
-print(type(of: j))  //String
+//: optional ?
+var h: Int? = nil
+h = 2
+//h + 3  //옵셔널 타입과 Int타입 연산 불가
+
+var str1 = "Hello"
+var str2 = "Swift"
+print(str1+str2)  //str1 - str2 빼기는 불가
+
+var arr: [Int] = [1, 2]  //같은 1가지 타입만 포함가능한 배열
+
+
+
+var num1 = 3
+var num2 = 5
+//:함수
+func addTwo1(num1:Int, num2: Int){
+    print(num1 + num2)
+}
+addTwo1(num1: num1, num2: num2)
+
+func addTwo2(num1 value1:Int, num2 value2: Int){
+    print(value1 + value2)
+}
+addTwo2(num1: num1, num2: num2)
+
+func addTwo3(_ value1:Int, _ value2: Int){
+    print(value1 + value2)
+}
+addTwo3(num1, num2)
